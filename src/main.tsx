26852a049575820fef8df64db83530974bdaf67f
王政 <everywangbug@gmail.com>
2024-09-05 17:29:58 +0800

    add storybook

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"
 import { App } from "./App.tsx"
 import "./global.scss"
 import "virtual:uno.css"
+import "unocss"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
