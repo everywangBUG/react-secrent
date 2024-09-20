782d9b00d561716ff665cf35e1c6c4e2d58e253e
王政 <everywangbug@gmail.com>
2024-09-20 14:21:58 +0800

    Slide right down从下面上边划入细节

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useCallback, useMemo } from "react"
+import { CSSProperties, PropsWithChildren, useCallback, useEffect, useMemo, useRef } from "react"
 import { animated, useTransition } from "@react-spring/web"
 
 const DURATION = 300
@@ -6,10 +6,43 @@ const DURATION = 300
 interface SlideProps extends PropsWithChildren {
   isVisible: boolean
   from?: "right" | "down"
+  className?: string
+  style?: CSSProperties
+  onEnter?: () => void
+  onExit?: () => void
 }
 
 export const Slide: React.FC<SlideProps> = (props) => {
-  const { isVisible, from = "right", children } = props
+  const { isVisible, from = "right", children, className, style, onEnter, onExit } = props
+  const visibleRef = useRef(isVisible)
+
+  useEffect(() => {
+    let timer = null
+    if (isVisible && onEnter !== null) {
+      timer = setTimeout(onEnter, DURATION)
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+    }
+  }, [isVisible, onEnter])
+
+  useEffect(() => {
+    let timer = null
+    if (!isVisible && onExit !== null && visibleRef.current) {
+      timer = setTimeout(onExit, DURATION)
+    }
+
+    visibleRef.current = isVisible
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+    }
+  })
 
   const x = useMemo(
     () => (
@@ -52,10 +85,13 @@ export const Slide: React.FC<SlideProps> = (props) => {
         transitions((props, isVisible) => (
           isVisible && (
             <animated.div
+              className={className}
               fixed top-0 right-0 bottom-0 left-0 bg="[rgba(0,0,0,0.3)]" z-10
-              style={{transform: props.x.to((x) => (x === 0 ? "none" : translate(x))), opacity: props.opacity}}
+              style={{
+                ...style,
+                transform: props.x.to((x) => (x === 0 ? "none" : translate(x))),
+                opacity: props.opacity}}
             >
-              <div h-100px>{JSON.stringify(props.x)}</div>
               {children}
             </animated.div>
           ))
--- src/pages/SlidePage.tsx
+++ src/pages/SlidePage.tsx
@@ -2,23 +2,46 @@ import { useState } from "react"
 import { Slide } from "../components/Slide"
 
 export const SlidePage: React.FC = () => {
-  const [show, setShow] = useState(false)
+  const [showRight, setShowRight] = useState(false)
+  const [showDown, setShowDown] = useState(false)
   
   return (
     <>
       <button
         type="button"
-        onClick={() => {setShow(true)}}
+        onClick={() => {setShowRight(true)}}
       >
-        开启
+        开启左边滑块
       </button>
-      <Slide 
-        isVisible={show}
+      <Slide
+        isVisible={showRight}
         from="right"
+        className="bg-blue-2"
+        style={{ border: "1px solid pink" }}
+        onEnter={() => alert("左边进入")}
+        onExit={() => alert("左边退出")}
       >
         <div>
-          <button type='button' onClick={() => {setShow(false)}}>
-            关闭
+          <button type='button' onClick={() => {setShowRight(false)}}>
+            关闭左边滑块
+          </button>
+        </div>
+      </Slide>
+      <button
+        type="button"
+        onClick={() => {setShowDown(true)}}
+      >
+        开启下面滑块
+      </button>
+      <Slide 
+        isVisible={showDown}
+        from="down"
+        className="bg-red-2"
+        style={{ border: "1px solid skyblue"}}
+      >
+        <div>
+          <button type='button' onClick={() => {setShowDown(false)}}>
+            关闭下面滑块
           </button>
         </div>
       </Slide>
