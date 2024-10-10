98f57e890f4d141924458f6c742e2ca1f3c6a7d2
王政 <everywangbug@gmail.com>
2024-10-10 12:43:08 +0800

    onBoarding 2

--- src/pages/OnBoarding.tsx
+++ src/pages/OnBoarding.tsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from "react"
 import { Mask } from "../components/OnBoarding/Mask"
+import { Popover } from "../components/Popover"
 
 export const OnBoarding: React.FC = () => {
+  const [element, setElement] = useState<HTMLElement | null>(null)
+  
+  useEffect(() => {
+    // 确保 DOM 已经加载完成
+    const targetElement = document.getElementById("xxx")
+    if (targetElement) {
+      setElement(targetElement)
+    } else {
+      console.error("Element with ID 'xxx' not found.")
+    }
+  }, [])
+
   return (
     <>
+    {
+      element && 
       <Mask
-        element={document.getElementById("xxx")!}
-        renderMaskContent={(wrapper) => {
-          return wrapper
-        }}
+        element={element}
+        renderMaskContent={() => {
+          return <Popover
+            open={true}
+            content={
+              <div style={{width: 300}}>
+                <p>hello</p>
+                <button b-none>下一步</button>
+              </div>
+            }
+          />
+      }}
       />
+    }
       <div id="xxx" w-100 m-auto>
         OnBoarding
         <div>1111</div>
